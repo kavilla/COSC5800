4ae47605e23e1e9bb7c4ea19ea9073ee28fae8a3
Kawika Avilla <kavilla414@gmail.com>
2019-11-20 21:45:40 -0600

    added routing.

--- src/app/src/components/Login.js
+++ src/app/src/components/Login.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import './App.css';
 
-class App extends React.Component {
+export default class Login extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            <code>Conference App</code>
+            <code>Login</code>
           </p>
         </header>
       </div>
-    )
+    );
   }
 }
-
-export default App;
