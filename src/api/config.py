0c01c9d58f7191ba934d98d45e34581afaebe210
Rocky Avilla <Kawika.Avilla@advicent.com>
2019-11-24 21:02:02 -0600

    stuck on the client issues

---
+++ src/api/config.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restplus import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+config = {
+    'db_username':  'avilla',
+    'db_password': '005711442',
+    'db_host': 'dataserv.mscsnet.mu.edu',
+    'db_port': '1521',
+    'db_sid': 'orcl'
+}
+
+api = Api(
+    title='Conference API',
+    version='1.0',
+    description='API for CS5800'
+)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://%s:%s@%s:%s/%s' % (config['db_username'], config['db_password'], config['db_host'], config['db_port'], config['db_sid'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SqlAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
